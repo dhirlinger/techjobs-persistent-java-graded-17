45da97c9faa227e0235b8bc6d4c872fe6b41e90f
Doug Hirlinger <doughirlinger@gmail.com>
2024-11-04 21:54:14 -0500

    task 3 tests passing except sql, pre-boot

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,6 +22,16 @@ import java.util.Optional;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+
     @RequestMapping("/")
     public String index(Model model) {
 
@@ -29,6 +44,7 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
 	model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        model.addAttribute("employers", employerRepository.findAll());
         return "add";
     }
 
@@ -41,7 +57,19 @@ public class HomeController {
             return "add";
         }
 
-        return "redirect:";
+        Optional<Employer> result = employerRepository.findById(employerId);
+        if (result.isEmpty()){
+            model.addAttribute("title", "Add Job");
+            return "add";
+        } else {
+            Employer employer = result.get();
+            newJob.setEmployer(employer);
+            jobRepository.save(newJob);
+            //model.addAttribute("jobs", jobRepository.findAll());
+
+
+            return "redirect:";
+        }
     }
 
     @GetMapping("view/{jobId}")
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,13 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +15,11 @@ public class Employer extends AbstractEntity {
     @NotNull(message = "Location is required.")
     @Size(min = 3, max = 100, message = "Location must be between 3 & 100 characters.")
     private String location;
+
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer(){}
 
